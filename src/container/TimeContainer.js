c32ec1cefab3843ce39ff9cc4dcdfb080fe6793e
Naveenkorkopp <Naveenkorkopp@users.noreply.github.com>
2017-11-17 15:56:36 +0530

    Some changes

---
+++ src/container/TimeContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import TimeComponent from '../Components/TimeComponent';
+
+class TimeContainer extends React.Component{
+	render(){
+		return (
+			<div>
+				<TimeComponent/>
+			</div>
+			);
+	}
+}
+
+export default TimeContainer;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TimeContainer from './container/TimeContainer';
 
 //create a component that output html
 const App = () => {
     return (
         <div>
-          Hello World
+          <h1>Hello World</h1>
+          <br />
+          <TimeContainer/>
         </div>
         );
 }
